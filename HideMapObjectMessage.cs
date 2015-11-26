6b3f0cc6340636c0751660bd4ad34650a0f9a827
Mathias Vetsch <m.vetsch@gmail.com>
2015-11-26 18:29:01 +0100

    added new Message types for MapObjects

---
+++ HideMapObjectMessage.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Drallo.MultiPlayer.Messages
+{
+    public class HideMapObjectMessage : IGameManagerMessage
+    {
+        [JsonProperty]
+        public Guid Id;
+
+        public HideMapObjectMessage(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ ShowMapObjectMessage.cs
@@ -0,0 +1,16 @@
+﻿using Drallo.ChallengeEngine.Feedback.Map;
+using Newtonsoft.Json;
+
+namespace Drallo.MultiPlayer.Messages
+{
+    public class ShowMapObjectMessage : IGameManagerMessage
+    {
+        [JsonProperty]
+        public IMapObject MapObject;
+
+        public ShowMapObjectMessage(IMapObject mapObject)
+        {
+            MapObject = mapObject;
+        }
+    }
+}
