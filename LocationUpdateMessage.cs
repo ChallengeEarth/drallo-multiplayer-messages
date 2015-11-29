359f4ed76d628b0db5cbf4e86432bbd156b4d8cb
Mathias Vetsch <m.vetsch@gmail.com>
2015-11-29 20:33:09 +0100

    Added Message for LocationUpdates

---
+++ LocationUpdateMessage.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Drallo.MultiPlayer.Messages
+{
+    public class LocationUpdateMessage : IGameManagerMessage
+    {
+        [JsonProperty]
+        public int PlayerId;
+
+        [JsonProperty]
+        public string UserName;
+
+        [JsonProperty]
+        public double Longitude;
+
+        [JsonProperty]
+        public double Latitude;
+
+        public LocationUpdateMessage(int playerId, string userName, double longitude, double latitude)
+        {
+            this.PlayerId = playerId;
+            this.UserName = userName;
+            this.Longitude = longitude;
+            this.Latitude = latitude;
+        }
+    }
+}
