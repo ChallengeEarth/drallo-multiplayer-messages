4f57218009412c806e66a1bb966325a7a3259242
Mathias Vetsch <m.vetsch@gmail.com>
2015-11-30 02:33:14 +0100

    added field for ranking in ChallengeFinishedMessage

--- ChallengeFinishedMessage.cs
+++ ChallengeFinishedMessage.cs
@@ -1,6 +1,16 @@
-﻿namespace Drallo.MultiPlayer.Messages
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Drallo.MultiPlayer.Messages
 {
     public class ChallengeFinishedMessage : IMessageData
     {
+        [JsonProperty]
+        public Dictionary<string, double> Ranking;
+
+        public ChallengeFinishedMessage(Dictionary<string, double> ranking)
+        {
+            this.Ranking = ranking;
+        }
     }
 }
