aa3825a0658d1a841cef2baaf957fb932de4fa8d
Mathias Vetsch <m.vetsch@gmail.com>
2015-12-03 19:30:11 +0100

    ranking is no longer part of ChallengeFinishedMessage

--- ChallengeFinishedMessage.cs
+++ ChallengeFinishedMessage.cs
@@ -1,16 +1,6 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
-
-namespace Drallo.MultiPlayer.Messages
+﻿namespace Drallo.MultiPlayer.Messages
 {
     public class ChallengeFinishedMessage : IMessageData
     {
-        [JsonProperty]
-        public Dictionary<string, double> Ranking;
-
-        public ChallengeFinishedMessage(Dictionary<string, double> ranking)
-        {
-            this.Ranking = ranking;
-        }
     }
 }
